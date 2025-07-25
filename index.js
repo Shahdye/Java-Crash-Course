37d59681a6915dc317765a4f92ea0c16a3d0bc91
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-07-25 10:18:54 -0400

    practice

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-let celsius = 10;
-let farenheit = celsius * 1.8 + 32;
-console.log(farenheit);
+let cash = 45
+let price = 40
+
+if (cash > price) {
+    console.log(`you paid extra - here's ${5} dollars change`)
+}
+
+else if (cash === price) {
+    console.log('you paid exact - have a nice day')
+}
+
+else (cash < price) {
+    console.log(`not enough money - you still owe me ${5} dollars`)
+}
