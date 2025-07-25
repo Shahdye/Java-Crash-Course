70593b46664513fafdcdcfcfc15d0e5cdb9fc453
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-07-25 10:24:25 -0400

    practice

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let cash = 45
-let price = 40
+let cash = 20;
+let price = 50;
+let difference = cash - price;
 
 if (cash > price) {
-    console.log(`you paid extra - here's ${5} dollars change`)
-}
-
-else if (cash === price) {
-    console.log('you paid exact - have a nice day')
-}
-
-else (cash < price) {
-    console.log(`not enough money - you still owe me ${5} dollars`)
+  console.log(`you paid extra - here's ${difference} dollars change`);
+} else if (cash === price) {
+  console.log("you paid exact - have a nice day");
+} else {
+  console.log(`not enough money - you still owe me ${difference} dollars`);
 }
