6cdd5cba6fad8787a51fe657219fea15ef7c7a2a
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-07-30 09:46:39 -0400

    updates

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-let cash = 20;
-let price = 50;
-let difference = cash - price;
+let cash = 50;
+let price = 40;
+let isStoreOpen = true;
 
-if (cash > price) {
-  console.log(`you paid extra - here's ${difference} dollars change`);
-} else if (cash === price) {
-  console.log("you paid exact - have a nice day");
-} else {
-  console.log(`not enough money - you still owe me ${difference * -1} dollars`);
+if (cash >= price && isStoreOpen === true) {
+  console.log(`print the receipt`);
 }
