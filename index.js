866d54da65ca0d015227346bf95424de5f8cc5a4
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-07-30 12:41:21 -0400

    practice

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-let cash = 50
-let price = 40
-let difference = cash - price
-let isStoreOpen = true
+let cash = 50;
+let price = 40;
+let difference = cash - price;
+let isStoreOpen = true;
 
-if (cash > price && isStoreOpen){
-    console.log(`give receipt`)
-}
-
-else (cash < price || isStoreOpen === false){
-    console.log('do not give receipt')
-}
+let str = cash >= price && isStoreOpen ? "give receipt" : "do not give receipt";
+console.log(str);
