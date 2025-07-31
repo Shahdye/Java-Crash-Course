7f62639f3b60c81c9adbb25126e899580997da6a
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-07-31 12:19:19 -0400

    updates

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-let cash = 50;
-let price = 40;
-let difference = cash - price;
-let isStoreOpen = true;
 
-let str = cash >= price && isStoreOpen ? "give receipt" : "do not give receipt";
-console.log(str);
