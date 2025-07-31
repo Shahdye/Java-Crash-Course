3bb4e1a39657735d7f6693bbd564387628ee1c51
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-07-31 14:07:08 -0400

    updates

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 for (let i = 1; i <= 20; i++) {
-  if (i % 3 === 0) {
-    console.log("Frontend");
-  }
-  if (i % 5 === 0) {
-    console.log("Simplified");
-  }
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("Frontend Simplified");
+  }
+  else if (i % 3 === 0) {
+    console.log("Frontend");
+  }
+  else if (i % 5 === 0) {
+    console.log("Simplified");
+  }
+  else{
+    console.log (i)
   }
 }
