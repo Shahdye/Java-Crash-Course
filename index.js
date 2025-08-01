efeed8193eeee639ec6647fc9b1cb96046d8e56a
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-01 13:07:28 -0400

    practice

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function convertCelsiusToFahrenheit(celsius) {
-  let fahrenheit = celsius * 1.8 + 32;
-  return fahrenheit;
+  return celsius * 1.8 + 32;
 }
 
 console.log(convertCelsiusToFahrenheit(0));
+console.log(convertCelsiusToFahrenheit(10));
+console.log(convertCelsiusToFahrenheit(30));
