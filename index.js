9b72db7adad1c3f3429d334c976e1c646977131c
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-04 11:41:19 -0400

    array practice

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let arr =[20, 30, 40, 50, 100]
+let arr = [20, 30, 40, 50, 100];
 
-console.log(arr[0])
+console.log(arr[0]);
 
-console.log(arr[arr.length -1])
+console.log(arr[arr.length - 1]);
 
+arr.push(200);
 
+console.log(arr);
