ad086226b7ce708dce0a21fdab4101149efe4dfb
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-06 10:14:47 -0400

    array practice

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-let grades = ["A+", "A", "FAIL"];
 
-let goodGrades = [];
-
-for (let i = 0; i < grades.length; ++i) {
-  if (grades[i] !== "FAIL") {
-    goodGrades.push(grades[i]);
-  }
-}
-
-console.log(goodGrades);
