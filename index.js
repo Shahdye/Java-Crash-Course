da4d1a073bff517e0ab92b7fb6f66e85983a9fab
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-06 10:34:02 -0400

    array practice

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let dollars = [1, 5, 10, 3];
 
-let cents = dollars.map((element) => {
-  return element * 100;
-});
+let cents = [];
+
+for (let i = 0; i < dollars.length; ++i) {
+  cents.push(dollars[i] * 100);
+}
 
 console.log(cents);
