6511807861d8a85a916ce5ccee15cdbb269d510e
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-07 13:38:54 -0400

    objects pratice

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let userFirstName = "David";
+let userLastName = "Bragg";
+let userDiscordID = "David Bragg#0001";
+let userSubsciptionStatus = "VIP";
 
+let user = {
+  username: "David",
+  email: "david@frontend.com",
+  subsciptionStatus: "VIP",
+  discordID: "David Bragg#0001",
+};
