393fa3011247adb40f6993c0f671842fc040c5aa
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-07 13:50:50 -0400

    objects practice

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-let user = {
-  username: "David",
-  email: "david@frontend.com",
-  subsciptionStatus: "VIP",
-  discordID: "David Bragg#0001",
-  lessonsCompleted: [0],
-};
+let users = [
+  {
+    username: "David",
+    email: "david@frontend.com",
+    subsciptionStatus: "VIP",
+    discordID: "David Bragg#0001",
+    lessonsCompleted: [0],
+  },
+  {
+    username: "Hank",
+    email: "Hank@frontend.com",
+    subsciptionStatus: "VIP",
+    discordID: "Hank Bragg#0001",
+    lessonsCompleted: [0, 1, 2],
+  },
+];
 
-console.log(user.username);
-
-console.log(user.email);
+console.log(users[0]);
