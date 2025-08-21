7be722454bfa3022391a7f4524435df287dfcac3
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-21 17:57:48 -0400

    promises practice

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const emailRef = document.querySelector(".email");
+console.log(emailRef);
+
 fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
   response.json().then((data) => {
     console.log(data);
+    emailRef.innerHTML = data.email;
   });
 });
