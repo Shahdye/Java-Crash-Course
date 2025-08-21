a0f4d2f4bd96541a8c3012b6a6052575acb350b0
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-21 12:21:51 -0400

    updates

--- index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#title").innerHTML += "FES SIMPLIFIED";
-
-document.querySelector("#title").style.fontSize = "10px";
-
-function changeTitleToRed() {
-  document.querySelector("#title").style.color = "red";
-}
