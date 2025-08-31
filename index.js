b359229a7a75beb1c8147a5eeff6f7e31bab5b5f
Emmanuel Shadi Rivera <emmanuelrivera9311@outlook.com>
2025-08-31 13:56:32 -0400

    async practice

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 async function main() {
-  await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = await response.json();
+  console.log(data);
+  emailRef.iinerHTML = data.email;
 }
 
 main();
